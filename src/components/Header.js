cd48ae449494444e6bc88ec15a8ded3fdff01598
amitaysoffer <amitaysof@gmail.com>
2021-02-05 10:09:18 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  const test = num => {
+    1 + 1;
+  };
+
+  const person = {
+    first: 'name',
+  };
+
+  return <header className="heyo asd">header</header>;
+};
+
+export default Header;
